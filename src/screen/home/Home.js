59a7349bc165b193cd36810995fb4afe8cb932b4
Mohan Prajapati <mohanprajapati0203@outlook.com>
2022-12-12 17:34:12 +0530

    Upcomming movies grid is added

--- src/screen/home/Home.js
+++ src/screen/home/Home.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 import Header from '../../common/header/Header'
+import "./Home.css"
+import Grid from '@mui/material/Grid';
+import moviesData from "../../common/moviesData";
+import { Container, Stack } from '@mui/system';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 export class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <p id="title">Upcoming Movies</p>
+                <div id="container">
+                    <div>
+                    {
+                        moviesData.map(data => {
+                            return (
+                                    <div className='movie-container'>
+                                        <img src={data.poster_url} alt={data.title} width="100%" height="250px" />
+                                        <p id="movie-title">{data.title}</p>
+                                    </div>
+                        )})}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Home
